10fec7ec404ca318f4ff970dbbe1570cbc7697b7
LooKing <LooKing070@gmail.com>
2024-04-14 22:20:05 +0300

    Hash funk test v0.00000000001

--- master_game/controllers/character_controller.py
+++ master_game/controllers/character_controller.py
@@ -24,44 +24,6 @@ def get_character(char_id):
     pass
 
 
-@app.route('/pers/<int:char_id>', methods=['GET'])
-def get_characters_weapon_id(char_id):
-    name, damage, typeOfDamage, distance, weight = "sword", 10, "bless", 6, 3
-    id = '0'
-    hashedName = hashing_string(name, 4, 3, -2)
-    hashedTOD = hashing_string(typeOfDamage, 4, 3, -2)
-    id += hashedName + hashedTOD
-    id += str(distance % 10)
-    id = max(-2147483648, min(int(id) + (damage * weight) // 2, 2147483647))
-    return id
-
-
-@app.route('/pers/<int:char_id>', methods=['GET'])
-def get_characters_armor_id(char_id):
-    name, armorClass, hindranceToSecrecy, strengthRequirement, weight = "leatherVest", "lats", True, 6, 3
-    id = str(int(hindranceToSecrecy))
-    hashedName = hashing_string(name, 6, 4, -2)
-    hashedAC = hashing_string(armorClass, 3, 3, -1)
-    id += hashedName.ljust(6, '0') + hashedAC.ljust(3, '0')
-    id = max(-2147483648, min(int(id) + (strengthRequirement + weight) // 2, 2147483647))
-    return id
-
-
-def hashing_string(string, needLenght, step, stepForLenght):
-    hashed, zagotovka = '', ''
-    for code in map(lambda x: str(ord(x)), string):
-        zagotovka += code
-    for i in range(1, len(zagotovka), step):
-        hashed += str((int(zagotovka[i]) + int(zagotovka[i - 1])) % 10)
-    while len(hashed) > needLenght:
-        hashed = str(int(hashed[:stepForLenght]) + int(hashed[stepForLenght:]))
-    return hashed.ljust(needLenght, '0')
-
-
-print(get_characters_weapon_id(-0))
-print(get_characters_armor_id(-0))
-
-
 @app.route('/class', methods=['GET'])
 def get_classes():  # Удалить?
     pass
--- master_game/services/character_service.py
+++ master_game/services/character_service.py
@@ -1,5 +1,5 @@
 from icecream import ic
-from master_game.models import CharacterSheet
+from master_game.models import CharacterSheet, Weapon, Armor
 from master_game.services import DatabaseService
 from master_game.services.cache_service import CacheService
 
@@ -53,3 +53,35 @@ class CharacterService:
         delete = f"user with id={character.id}"
         ic(delete)
 
+    def get_characters_weapon_id(self, weapon: Weapon) -> int:
+        charactersWeapon = self._session.query(weapon).to_dict()
+        name, typeOfDamage = charactersWeapon["name"], charactersWeapon["typeOfDamage"]
+        damage, distance, weight = charactersWeapon["damage"], charactersWeapon["distance"], charactersWeapon["weight"]
+        hashedName = CharacterService.hashing_string(name)
+        hashedTOD = CharacterService.hashing_string(name, 19_999_999)
+        hashedId = hashedName + hashedTOD + (damage ^ weight) + round(distance / 10)  # убрать ^ ?
+        return hashedId  # [0;2_147_483_647]
+
+    def get_characters_armor_id(self, armor: Armor) -> int:
+        charactersWeapon = self._session.query(armor).to_dict()
+        name, armorClass = charactersWeapon["name"], charactersWeapon["armorClass"]
+        hindranceToSecrecy = int(charactersWeapon["hindranceToSecrecy"])
+        strengthRequirement, weight = charactersWeapon["strengthRequirement"], charactersWeapon["weight"]
+        hashedId = hindranceToSecrecy * (2_147_483_646 // 2)
+        hashedName = CharacterService.hashing_string(name, 2 - hindranceToSecrecy)
+        hashedAC = CharacterService.hashing_string(armorClass, 1_999_999)
+        hashedId += hashedName + hashedAC + strengthRequirement + weight
+        return hashedId  # [0;2_147_483_647]
+
+    @staticmethod
+    def hashing_string(string, thresholdCoeff=1) -> int:
+        hashed = 2_147_483_647
+        billet = ''
+        symbolsCodes = [code for code in map(ord, string)]
+        for c in symbolsCodes:
+            billet += str(c)
+        while hashed > 2_000_000_000 // (1 + thresholdCoeff):
+            hbl = len(billet) // 2
+            hashed = int(billet[:hbl]) ^ int(billet[hbl:])
+            billet = str(hashed)
+        return hashed
