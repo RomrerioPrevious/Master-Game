bcf636f3732840b0f2dbb4fed974f05e8e4d0065
Dmitry <139960231+dima117216@users.noreply.github.com>
2024-04-16 22:50:26 +0300

    Update AuthificationController.py

--- master_game/controllers/AuthificationController.py
+++ master_game/controllers/AuthificationController.py
@@ -1,42 +1,62 @@
 import random
-
-from flask import Flask, request, jsonify
-import sqlite3
+from flask import Flask, render_template, url_for, request
+from user import User
+from user_service import UserService
 
 app = Flask(__name__)
+service = UserService()
 
-conn = sqlite3.connect('db/base.db')
-cursor = conn.cursor()
+
+def hash_password(password):
+    hash_value = 0
+    for char in password:
+        hash_value = (hash_value << 5) - hash_value + ord(char)
+        hash_value = hash_value & 0xFFFFFFFF
+    return hash_value
+
+
+def form():
+    return "/form.html"
 
 
 @app.route('/auth', methods=['GET'])
 def auth():
     # TODO: authentication
-    return jsonify({'message': 'Authentication successful'})
+    return render_template("/page.html")
 
 
 @app.route('/user', methods=['POST'])
 def create_user():
-    user_data = request.json
-    new_user_id = random.randint(10000, 99999)
-    # TODO: creation account
-    return jsonify({'message': f"User created successfully. User's id: {new_user_id}"})
+    form()
+    name = request.form["account_name"]
+    email = request.form["email"]
+    age = request.form["age"]
+    password = request.form["password"]
+    status = request.form["status"]
+    id = random.randint(10000, 99999)
+    # filled add_user as example, change later
+    service.add_user(id,
+                     User(id=id, username=name, email=email, age=int(age), password=hash_password(password),
+                          status=status, sheets=[]))
 
 
 @app.route('/user/<int:user_id>', methods=['PATCH'])
 def update_user(user_id):
-    user_data = request.json
-    # TODO: update users
-    return jsonify({'message': 'User updated successfully'})
+    password = ""
+    hash_value = 0
+    for char in password:
+        hash_value = (hash_value << 5) - hash_value + ord(char)
+        hash_value = hash_value & 0xFFFFFFFF
+    hashed_password = hash_value
+    service.update_user(user_id,
+                        User(id=user_id, username="", email="", age=0, password=hashed_password, status="", sheets=[]))
 
 
 @app.route('/user/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    # TODO: delete users
-    return jsonify({'message': 'User deleted successfully'})
+    service.delete_user(user_id)
 
 
 @app.route('/user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    # TODO: get users
-    return jsonify({'user_id': user_id, 'name': 'Test User'})
+    service.get_user(user_id)
