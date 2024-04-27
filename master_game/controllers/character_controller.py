58e7a8903cae85a7fc2f245a3f7ce1fc4d8eca12
LooKing <LooKing070@gmail.com>
2024-04-27 17:34:45 +0300

    character_controller.py v0.000001

--- master_game/controllers/character_controller.py
+++ master_game/controllers/character_controller.py
@@ -72,25 +72,10 @@ def update_character(char_id):
 @app.route("/pers/<int:char_id>", methods=["DELETE"])
 def delete_character(char_id):
     user_id = int(request.cookies.get("user_id"))
-    character = CharacterService().get_character(char_id)
     user = UserService().get_user_by_id(user_id)
     character = character_service.get_character(char_id)
     if user["status"] == "admin" or character in user["sheets"]:
         CharacterService().delete_character(char_id)
         character_service.delete_character(char_id)
         user["sheets"].remove(character)
-    UserService().update_user(user)
-
-
-@app.route('/pers/<int:char_id>', methods=['GET'])
-def get_character(char_id):
-    user_id = int(request.cookies.get("user_id"))
-    try:
-        user = UserService().get_user(user_id)
-        character = CharacterService().get_character(char_id)
-        if character not in user["sheets"]:
-            raise Exception(f"Not found character with id={char_id}")
-    except Exception:
-        return
-    return character
     user_service.update_user(user)
