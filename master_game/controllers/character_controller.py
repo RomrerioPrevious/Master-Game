d4c0c939c78bbd32dcd5c3df987a6bb29d420423
Dmitry <139960231+dima117216@users.noreply.github.com>
2024-04-27 17:03:06 +0300

    Update character_controller.py

--- master_game/controllers/character_controller.py
+++ master_game/controllers/character_controller.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, request, redirect, url_for
+from master_game.services.character_service import CharacterService
+from master_game.services.user_service import UserService
 from master_game.models.character.character_sheet import CharacterSheet
+from master_game.config import Logger
+from flask import Blueprint, request, redirect, url_for
 from master_game.models.character.weapon import Weapon
 from master_game.models.character.armor import Armor
 from master_game.models.character.stats import Stats
-from master_game.services.character_service import CharacterService
 import logging
 
+app = Blueprint("character", __name__)
+character_service = CharacterService()
+user_service = UserService()
 logger = logging.getLogger("Logger")
 logger.setLevel(logging.ERROR)
-app = Blueprint("character", __name__)
-characterService = CharacterService()
 
 
 def inf():
@@ -31,11 +34,24 @@ def inf():
     return character
 
 
+@app.route("/pers/<int:char_id>", methods=["GET"])
+def get_character(char_id):
+    user_id = int(request.cookies.get("user_id"))
+    try:
+        user = user_service.get_user_by_id(user_id)
+        character = character_service.get_character(char_id)
+        if character not in user["sheets"]:
+            raise Exception(f"Not found character with id={char_id}")
+        return character
+    except Exception as ex:
+        Logger.write_error(ex)
+
+
 @app.route('/pers', methods=['POST'])
 def create_character():
     try:
         character = inf()
-        characterService.add_character(character)
+        character_service.add_character(character)
         return redirect(url_for('character_page', char_id=character.id))
     except Exception as e:
         print(e)
@@ -46,7 +62,7 @@ def create_character():
 def update_character(char_id):
     try:
         character = inf()
-        characterService.update_character(character)
+        character_service.update_character(character)
         return redirect(url_for('character_page', charid=char_id))
     except Exception as e:
         print(e)
@@ -54,35 +70,29 @@ def update_character(char_id):
 
 
 @app.route('/pers/<int:char_id>', methods=['DELETE'])
+@app.route("/pers/<int:char_id>", methods=["DELETE"])
 def delete_character(char_id):
-    pass
+    user_id = int(request.cookies.get("user_id"))
+    user = UserService().get_user(user_id)
+    character = CharacterService().get_character(char_id)
+    user = UserService().get_user_by_id(user_id)
+    character = character_service.get_character(char_id)
+    if user["status"] == "admin" or character in user["sheets"]:
+        CharacterService().delete_character(char_id)
+        character_service.delete_character(char_id)
+        user["sheets"].remove(character)
+    UserService().update_user(user)
 
 
 @app.route('/pers/<int:char_id>', methods=['GET'])
 def get_character(char_id):
-    pass
-
-
-@app.route('/class', methods=['GET'])
-def get_classes():  # Удалить?
-    pass
-
-
-@app.route('/species', methods=['GET'])
-def get_species():  # Удалить?
-    pass
-
-
-@app.route('/random', methods=['GET'])
-def get_random_character():  # Удалить?
-    pass
-
-
-@app.route('/random/cube', methods=['GET'])
-def roll_cube():  # Удалить?
-    pass
-
-
-@app.route('/random/items', methods=['GET'])
-def get_random_items():  # Удалить?
-    pass
+    user_id = int(request.cookies.get("user_id"))
+    try:
+        user = UserService().get_user(user_id)
+        character = CharacterService().get_character(char_id)
+        if character not in user["sheets"]:
+            raise Exception(f"Not found character with id={char_id}")
+    except Exception:
+        return
+    return character
+    user_service.update_user(user)
